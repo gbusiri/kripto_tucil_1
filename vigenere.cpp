e19f2bdee9ff4496cfe043c2f7d0afcae460bef6
Moch Ginanjar Busiri <gbusiri@gmail.com>
2016-09-01 14:30:28 +0700

    refactor vigenere standard

--- vigenere.cpp
+++ vigenere.cpp
@@ -1,4 +1,5 @@
 #include <bits/stdc++.h>
+#include "util.h"
 using namespace std;
 
 const int N = 30;
@@ -59,46 +60,123 @@ string decrypt(string cipher, string key) {
 	return plain;
 }
 
-int main() {
-	string plain, key, cipher, cipher_non_space = "", cipher_five = "";
-	int * lengthOfBytesFile;
-	ofstream file, file_plain, file_cipher;
+void do_encrypt() {
+    string plain = get_input();
+    string key = get_key();
+    instance_table();
+    string cipher = encrypt(plain, key);
 
-	instance_table();
+    cout << "==== Apa Adanya : ";
+	cout << cipher << endl;
 
-	cout << "Plain : ";
-	getline(cin, plain);
-
-	cout << "Key : ";
-	cin >> key;
-
-	file.open("output.txt");
-	cout << "Cipher : " << endl;
-
-	cipher = encrypt(plain, key);
-	cout << "==== Apa adanya : " << cipher << endl;
-	file << "==== Apa adanya : " << cipher << endl;
-	cout << "==== Tanpa spasi : ";
-	file << "==== Tanpa spasi : ";
+	string tanpa_spasi;
 	for (int i = 0; i < cipher.size(); ++i)
-	{
-		if (isAlphabet(cipher[i])) {
-			cipher_non_space += cipher[i];
-		}
-	}
-	cout << cipher_non_space << endl;
-	file << cipher_non_space + "\n";
+		if (cipher[i] != ' ') tanpa_spasi += cipher[i];
+	cout << "==== Tanpa Spasi : " << tanpa_spasi << endl;
 
 	cout << "==== Kelompok 5 huruf : ";
-	file << "==== Kelompok 5 huruf : ";
-	for (int i = 0; i < cipher_non_space.size(); i+=5)
-	{
-		cipher_five += cipher_non_space.substr(i, 5) + " ";
+	for (int i = 0; i < tanpa_spasi.size(); i += 5) {
+		cout << tanpa_spasi.substr(i, 5);
+		cout << (i + 5 >= tanpa_spasi.size() ? '\n' : ' ');
 	}
-	cout << cipher_five << endl;
-	file << cipher_five + "\n";
-	file.close();
-	cout << "Plain Again From Cipher = " << decrypt(cipher, key) << endl;
 
+    const string file_name = "cipher.txt";
+    ofstream f(file_name.c_str(), ios::out|ios::binary);
+    f << cipher;
+    f.close();
+
+    cout << "\n";
+    cout << "Ciphertext tersimpan dalam file " << file_name << endl;
+}
+
+void do_decrypt() {
+    string cipher = get_input();
+    string key = get_key();
+    instance_table();
+    string plain = decrypt(cipher, key);
+
+    cout << "\n";
+    cout << "Cipher:\n";
+    cout << cipher << endl;
+
+    cout << "\n";
+    cout << "Plain:\n";
+    cout << plain << endl;
+
+    const string file_name = "plain.txt";
+    ofstream f(file_name.c_str(), ios::out|ios::binary);
+    f << plain;
+    f.close();
+
+    cout << "\n";
+    cout << "Plaintext tersimpan dalam file " << file_name << endl;
+}
+
+void menu() {
+    cout << "Vigenere Cipher Standard\n";
+    while (1) {
+        cout << "\nPilihan menu:\n";
+        cout << "1. Enkripsi\n";
+        cout << "2. Dekripsi\n";
+        cout << "0. Keluar\n";
+        cout << "> ";
+        int menu; cin >> menu;
+        char c; cin.get(c); // ignore newline
+        if (menu == 0) break;
+        switch (menu) {
+            case 1:
+                do_encrypt();
+                break;
+            case 2:
+                do_decrypt();
+                break;
+            default:
+                cout << "Pilihan salah!\n";
+        }
+    }
+}
+
+int main() {
+	// string plain, key, cipher, cipher_non_space = "", cipher_five = "";
+	// int * lengthOfBytesFile;
+	// ofstream file, file_plain, file_cipher;
+
+	// instance_table();
+
+	// cout << "Plain : ";
+	// getline(cin, plain);
+
+	// cout << "Key : ";
+	// cin >> key;
+
+	// file.open("output.txt");
+	// cout << "Cipher : " << endl;
+
+	// cipher = encrypt(plain, key);
+	// cout << "==== Apa adanya : " << cipher << endl;
+	// file << "==== Apa adanya : " << cipher << endl;
+	// cout << "==== Tanpa spasi : ";
+	// file << "==== Tanpa spasi : ";
+	// for (int i = 0; i < cipher.size(); ++i)
+	// {
+	// 	if (isAlphabet(cipher[i])) {
+	// 		cipher_non_space += cipher[i];
+	// 	}
+	// }
+	// cout << cipher_non_space << endl;
+	// file << cipher_non_space + "\n";
+
+	// cout << "==== Kelompok 5 huruf : ";
+	// file << "==== Kelompok 5 huruf : ";
+	// for (int i = 0; i < cipher_non_space.size(); i+=5)
+	// {
+	// 	cipher_five += cipher_non_space.substr(i, 5) + " ";
+	// }
+	// cout << cipher_five << endl;
+	// file << cipher_five + "\n";
+	// file.close();
+	// cout << "Plain Again From Cipher = " << decrypt(cipher, key) << endl;
+
+	menu();
 	return 0;
 }
